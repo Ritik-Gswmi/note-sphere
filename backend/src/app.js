656508e875e3462d91fee4690289d8716f3540e0
ritik <ritikk@gmail.com>
2026-04-22 22:10:47 +0530

    Update app.js for a better msg on root route

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("NoteSphere API is running 🚀");
+});
+
 app.use("/api/auth", require("./routes/auth.routes.js"));
 app.use("/api/users", require("./routes/user.routes.js"));
 app.use("/api/notes", require("./routes/note.routes.js"));
